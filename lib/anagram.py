90f332984ff58b19dc2224b8d727a440923abc18
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-23 13:56:10 -0700

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram: 
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, match_list): 
+        return [test_word for test_word in match_list if (sorted(self.word) == sorted(test_word))]
